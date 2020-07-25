129ccd4d4efc59d89350455e25fdfd488b3c5c2d
danielhenrymantilla <danielhenrymantilla@users.noreply.github.com>
2020-07-25 23:03:11 +0000

    deploy: e77358cdd0c4deab0af54ac96b04e561be0f8101

---
+++ 0.1.1/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"bstringify":{"doc":"`bstringify!`","i":[[14,"bstringify","bstringify","",null,null]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ 0.1.1/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bstringify"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
